50abc8f7509f83dd9fd9d96f4633c596d11d6d6a
akejutaofeek5-dev <akejutaofeek5@gmail.com>
2026-09-10 20:28:48 +0100

    Fix Vercel client import paths

--- client/App.tsx
+++ client/App.tsx
@@ -2,9 +2,9 @@ import "./global.css";
 
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import PublicPage from "./pages/PublicPage";
+import Index from "./pages/Index.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import PublicPage from "./pages/PublicPage.tsx";
 
 const App = () => (
   <BrowserRouter>
